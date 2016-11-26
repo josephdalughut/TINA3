d733ad4a47fa67e53613bc6f10b27658dd0e9007
Joey Dalu <joseph.dalughut@gmail.com>
2016-11-26 23:24:02 +0100

    enabled cgi on rf server

--- server/rf/send.py
+++ server/rf/send.py
@@ -10,6 +10,7 @@ import cgitb
 cgitb.enable()
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
 
@@ -31,44 +32,45 @@ radio.openReadingPipe(1, pipes[1])
 # radio.startListening()
 if len(sys.argv) != 2:
     print ("ERROR: missing args")
-    sys.exit(0)
-message = list(str(sys.argv[1]))
-while len(message) < 32:
-    message.append(0)
-retry=0
-while(retry < 5):
-    start = time.time()
-    radio.write(message)
-    #print("Sent the message: {}".format(message))
-    radio.startListening()
-    while not radio.available(0):
-        time.sleep(1 / 100)
-        if time.time() - start > 2:
-            #print("Timed out.")
-            break
+else:
+    message = list(str(sys.argv[1]))
+    returned=1
+    while len(message) < 32:
+        message.append(0)
+    retry=0
+    while(retry < 5 and returned==1):
+        start = time.time()
+        radio.write(message)
+        #print("Sent the message: {}".format(message))
+        radio.startListening()
+        while not radio.available(0):
+            time.sleep(1 / 100)
+            if time.time() - start > 2:
+                #print("Timed out.")
+                break
 
-    receivedMessage = []
-    radio.read(receivedMessage, radio.getDynamicPayloadSize())
-    #print("Received: {}".format(receivedMessage))
-    string = ""
-    for n in receivedMessage:
-        # Decode into standard unicode set
-        if (n >= 32 and n <= 126):
-            string += chr(n)
-    #print("Out received message decodes to: {}".format(string))
-    if len(string) > 1:
-        #print ("Callback received:")
-        print string
+        receivedMessage = []
+        radio.read(receivedMessage, radio.getDynamicPayloadSize())
+        #print("Received: {}".format(receivedMessage))
+        string = ""
+        for n in receivedMessage:
+            # Decode into standard unicode set
+            if (n >= 32 and n <= 126):
+                string += chr(n)
+        #print("Out received message decodes to: {}".format(string))
+        if len(string) > 1:
+            #print ("Callback received:")
+            print string
+            returned=0
+            radio.write("")
+            #sys.stdout.write(string);
+    	#print ("ERROR: No Response")
+    	#sys.exit(0)
         radio.stopListening()
-        radio.write("")
-        radio.powerDown()
-        sys.exit(0)
-        #sys.stdout.write(string);
-	#print ("ERROR: No Response")
-    radio.stopListening()
-    retry = retry + 1
-    time.sleep(1/100)
-print ("ERROR: timed out")
+        retry = retry + 1
+        time.sleep(1/100)
+    if returned != 0:
+        print ("ERROR: timed out")
 radio.stopListening()
 radio.powerDown()
-sys.exit(0)
+#sys.exit(0)
