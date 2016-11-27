e223a4b9b2bc14af25211ada0b987a77b7fa0b8f
Joey Dalu <joseph.dalughut@gmail.com>
2016-11-27 08:35:46 +0100

    update

--- server/rest/api/SmartPlugApi.php
+++ server/rest/api/SmartPlugApi.php
@@ -54,7 +54,7 @@ class SmartPlugApi extends AbstractApi
         $script = "python ../../rf/send.py \"".$command."\"";
         $result = shell_exec($script);
         $VAL = "OFF";
-        if (preg_match('/^ERROR/', $result)) {
+        /*if (preg_match('/^ERROR/', $result)) {
             return $this->_response("Not found, return successful from script with error: ".$result, HTTPStatusCode::$NOT_FOUND);
         } else{
             $arr = $this->_responseToArr($result);
@@ -75,7 +75,8 @@ class SmartPlugApi extends AbstractApi
         if(!$this->_getDatabase()->query($insertSQL)){
             return $this->_response("Failed", HTTPStatusCode::$SERVICE_UNAVAILABLE);
         }
-        return $this->_response($smartPlug, HTTPStatusCode::$OK);
+        */
+        return $this->_response($result, HTTPStatusCode::$NOT_FOUND);
     }
 
     public function testPy($args){
